b2058ae64c44ae49f5af9722d46fef5322ed7140
Roland McRae <mcraeroland@gmail.com>
2020-04-24 08:47:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(array)
   array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   array.all? do |word|
     word.length <= 4
   end
